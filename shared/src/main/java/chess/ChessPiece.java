e6f178cdcd22f1fdbcc0d681ce6f5b906b0ee58d
Claire Daley <clairedaley123@icloud.com>
2026-01-16 17:04:31 -0700

    add pieceColor and type.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -14,6 +14,7 @@ public class ChessPiece {
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.pieceColor = pieceColor;
         this.type = type;
+
     }
 
     /**
