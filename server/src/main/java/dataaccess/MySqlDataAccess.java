b3f073be79b7c310609563e96bc6ea269c7b2b75
Claire Daley <clairedaley123@icloud.com>
2026-03-09 21:42:38 -0600

    Delete files so there's only one sql file for dataaccess.Set up methods for implementing data access in my sl data access file.

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -10,8 +10,9 @@ import org.mindrot.jbcrypt.BCrypt;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
-public class MySqlDataAccess {
+public class MySqlDataAccess implements DataAccess {
     private final Gson gson = new Gson();
     private static final String[] CREATE_STATEMENTS = {
             """
@@ -38,4 +39,79 @@ public class MySqlDataAccess {
                 )
             """
     };
+
+    public MySqlDataAccess() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection connect = DatabaseManager.getConnection()) {
+            for (String statement: CREATE_STATEMENTS) {
+                try (PreparedStatement prep = connect.prepareStatement(statement)) {
+                    prep.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Failed to initialize database: " + e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        String[] tables = {"auth", "game", "user"};
+        try (Connection connect = DatabaseManager.getConnection()) {
+            for (String table:tables) {
+                try (PreparedStatement prep = connect.prepareStatement("DELETE FROM " + table)) {
+                    prep.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: " + e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+
+    }
+
+    @Override
+    public int createGame(String gameName) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Collection<GameData> listGames() throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+
+    }
+
+    //Users
+    @Override
 }
