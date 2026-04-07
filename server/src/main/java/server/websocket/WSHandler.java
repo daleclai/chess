51188883e6e135e1873f7de2197bf4f99b3df0d2
Claire Daley <clairedaley123@icloud.com>
2026-04-06 22:18:38 -0600

    add handlers and check conditions players/observers might be in.

--- server/src/main/java/server/websocket/WSHandler.java
+++ server/src/main/java/server/websocket/WSHandler.java
@@ -1,6 +1,6 @@
 package server.websocket;
 
-import chess.*;
+import chess.ChessGame;
 import com.google.gson.Gson;
 import dataaccess.*;
 import io.javalin.websocket.*;
@@ -45,7 +45,7 @@ public class WSHandler extends WsConfig {
             switch (command.getCommandType()) {
                 case CONNECT -> handleConnect(ctx, command);
                 case MAKE_MOVE -> {
-                    MakeMoveCommand moveCommand = gson.fromJson(ctx.message(), MakeMoveCommand.class);
+                    MoveCommand moveCommand = gson.fromJson(ctx.message(), MakeMoveCommand.class);
                     handleMakeMove(ctx, moveCommand);
                 }
                 case LEAVE -> handleLeave(ctx, command);
@@ -54,6 +54,86 @@ public class WSHandler extends WsConfig {
         } catch (Exception e) {
             sendError(ctx, "Error: " + e.getMessage());
         }
+    }
+
+    //handlers
+
+    private void handleConnect(WsMessageContext ctx, UserGameCommand command) throws DataAccessException, IOException {
+        AuthData auth = dataAccess.getAuth(command.getAuthToken());
+        if (auth == null) {
+            sendError(ctx, "Error: unauthorized");
+            return;
         }
+
+        GameData game = dataAccess.getGame(command.getGameID());
+        if (game==null) {
+            sendError(ctx, "Error: game not found");
+            return;
+        }
+
+        connections.add(command.getGameID(), ctx.session());
+
+        //send load game to client
+        String loadMsg = gson.toJson(new LoadGame(game));
+        connections.sendToRoot(ctx.session(), loadMsg);
+
+        String username = auth.username();
+        String role;
+        if (username.equals(game.whiteUsername())) {
+            role = username + " joined as WHITE";
+        } else if (username.equals(game.blackUsername())) {
+            role = username + " joined as BLACK";
+        } else {
+            role = username = " joined as observer";
+        }
+        String notifyMsg = gson.toJson(new Notification(role));
+        connections.broadcast(command.getGameID(), ctx.session, notifyMsg);
+    }
+
+
+    private void handleMakeMove(WsMessageContext ctx, MoveCommand command) throws DataAccessException, IOException {
+        AuthData auth = dataAccess.getAuth(command.getAuthToken());
+        if (auth == null) {
+            sendError(ctx, "Error: unauthorized");
+            return;
+        }
+
+        GameData gameData = dataAccess.getGame(command.getGameID());
+        if (gameData == null) {
+            sendError(ctx, "Error: game not found");
+        }
+        ChessGame game = gameData.game();
+
+        //check if player is in checkmate or stalemate
+
+        if (
+                game.isInCheckmate(ChessGame.TeamColor.WHITE) ||
+                game.isInCheckmate(ChessGame.TeamColor.BLACK) ||
+                game.isInStalemate(ChessGame.TeamColor.WHITE) ||
+                game.isInStalemate(ChessGame.TeamColor.BLACK)
+        ) {
+            sendError(ctx, "Error: game is already over");
+            return;
+        }
+
+        //check whose turn it is
+        String username = auth.username();
+        ChessGame.TeamColor playerColor = null;
+        if (username.equals(gameData.whiteUsername())) {
+            playerColor = ChessGame.TeamColor.WHITE;
+        } else if (username.equals(gameData.blackUsername())) {
+            playerColor = ChessGame.TeamColor.BLACK;
+        } else {
+            sendError(ctx, "Error: observers cannot make moves");
+            return;
+        }
+
+        if (game.getTeamTurn() != playerColor) {
+            sendError(ctx, "Error: it is not your turn");
+            return;
+        }
+    }
+
+    private void sendError(WsMessageContext ctx, String s) {
     }
 }
