409a316e5824b7ef56ffc981522651e12f178469
Claire Daley <clairedaley123@icloud.com>
2026-04-08 16:30:24 -0600

    fix files and switch to javax.websocket with tyrus.

--- client/src/main/java/ui/WSFacade.java
+++ client/src/main/java/ui/WSFacade.java
@@ -1,55 +1,74 @@
 package ui;
 
 import com.google.gson.Gson;
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.handshake.ServerHandshake;
 import websocket.commands.MoveCommand;
 import websocket.commands.UserGameCommand;
 import websocket.messages.ServerMessage;
 
+import javax.websocket.*;
 import java.net.URI;
 
-public class WSFacade extends WebSocketClient {
+@ClientEndpoint
+public class WSFacade {
 
+    private Session session;
     private final Gson gson = new Gson();
     private final ServerMessageObserver observer;
+
     public WSFacade(int port, ServerMessageObserver observer) throws Exception {
-        super(new URI("ws://localhost:" + port + "/ws"));
         this.observer = observer;
-        connectBlocking();
+        URI uri = new URI("ws://localhost:" + port + "/ws");
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        container.connectToServer(this, uri);
     }
-    @Override
-    public void onOpen(ServerHandshake handshake) {
+
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
     }
-    @Override
+
+    @OnMessage
     public void onMessage(String message) {
         ServerMessage serverMessage = gson.fromJson(message, ServerMessage.class);
         observer.notify(serverMessage);
     }
-    @Override
-    public void onClose(int code, String reason, boolean remote) {
+
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        System.err.println("WebSocket error: " + throwable.getMessage());
     }
-    @Override
-    public void onError(Exception e) {
-        System.err.println("WebSocket error: " + e.getMessage());
+
+    @OnClose
+    public void onClose(Session session, CloseReason reason) {
+        this.session = null;
     }
+
     public void sendConnect(String authToken, int gameID) throws Exception {
         UserGameCommand command = new UserGameCommand(
                 UserGameCommand.CommandType.CONNECT, authToken, gameID);
-        send(gson.toJson(command));
+        session.getBasicRemote().sendText(gson.toJson(command));
     }
+
     public void makeMove(String authToken, int gameID, chess.ChessMove move) throws Exception {
         MoveCommand command = new MoveCommand(authToken, gameID, move);
-        send(gson.toJson(command));
+        session.getBasicRemote().sendText(gson.toJson(command));
     }
+
     public void leave(String authToken, int gameID) throws Exception {
         UserGameCommand command = new UserGameCommand(
                 UserGameCommand.CommandType.LEAVE, authToken, gameID);
-        send(gson.toJson(command));
+        session.getBasicRemote().sendText(gson.toJson(command));
     }
+
     public void resign(String authToken, int gameID) throws Exception {
         UserGameCommand command = new UserGameCommand(
                 UserGameCommand.CommandType.RESIGN, authToken, gameID);
-        send(gson.toJson(command));
+        session.getBasicRemote().sendText(gson.toJson(command));
+    }
+
+    public void close() throws Exception {
+        if (session != null && session.isOpen()) {
+            session.close();
+        }
     }
 }
